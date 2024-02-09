0aa9b73073e6fd6f62d32a0dc315ffcbae92b92b
Sardorbek <sardorbek.saydamatov@mail.ru>
2024-02-09 23:04:42 +0500

    TabView

--- UnitedFuelFinder/App/Views/TabView/MainTabView.swift
+++ UnitedFuelFinder/App/Views/TabView/MainTabView.swift
@@ -36,7 +36,7 @@ struct MainTabView: View {
                     Text("map".localize)
                 }
             
-            Text("Page3")
+            SettingsView()
                 .tabItem {
                     Image("icon_settings")
                         .renderingMode(.template)
